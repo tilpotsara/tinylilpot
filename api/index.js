c4fdcfd6dd38460d39b2f16f8633d0eb1075eee2
tilpotsara <95207382+tilpotsara@users.noreply.github.com>
2021-11-29 09:56:08 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,34 @@
+import dotenv from 'dotenv'
+dotenv.config();
 import express from 'express'
 import requestIp from 'request-ip'
+import { createClient } from '@supabase/supabase-js';
+const supabase = createClient(
+    process.env.TRILLYDB,
+    process.env.TRILLYDB_KEY,
+    {
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: true
+    })
 const app = express()
 app.use(requestIp.mw())
-
+async function upload(ip, sourc) {
+    const { data, error } = await supabase
+        .from('trackevents')
+        .insert([{ ipaddress: ip, source: sourc }]);
+    console.log(error);
+}
 const trackImg = Buffer.from('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64');
-const port = 80;
 app.get('/api/:campaign/:id', (req, res) => {
     res.writeHead(200, {
         'Content-Type': 'image/gif',
         'Content-Length': trackImg.length
     })
     console.log(`${req.method} ${req.url} - ${req.socket.remoteAddress} - ${req.clientIp}`);
+    upload(req.clientIp || req.socket.remoteAddress, req.path);
     res.end(trackImg)
 })
-app.get('*', (req, res) => {
-    res.redirect('https://www.google.com');
-});
-app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`)
+app.listen(process.env.port, () => {
+    console.log(`listening at http://localhost:${process.env.port}`)
 })
