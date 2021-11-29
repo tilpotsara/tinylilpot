664e893de63d9cb107770c34032d2ac51ee95a74
tilpotsara <95207382+tilpotsara@users.noreply.github.com>
2021-11-29 11:44:24 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ dotenv.config();
 import express from 'express'
 import requestIp from 'request-ip'
 import { createClient } from '@supabase/supabase-js';
+import geoip from 'geoip-lite';
+
 const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY,
@@ -13,20 +15,33 @@ const supabase = createClient(
     })
 const app = express()
 app.use(requestIp.mw())
-async function upload(ip, sourc) {
+async function upload(opened) {
     const { data, error } = await supabase
         .from('trackevents')
-        .insert([{ ipaddress: ip, source: sourc }]);
+        .insert([opened]);
     console.log(error);
 }
 const trackImg = Buffer.from('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64');
 app.get('/api/:campaign/:id', (req, res) => {
+    const IP = req.clientIp || req.socket.remoteAddress;
+    console.log(`${req.method} ${req.url} - ${IP}`);
+
+    const visit = {
+        ipaddress: IP, source: req.path,
+        client: req.headers["user-agent"],
+        lng: req.headers["accept-language"],
+    }
+    if (IP) {
+        const geo = geoip.lookup(IP);
+        if (geo)
+            visit.geo = JSON.stringify(geo);
+    }
+
+    upload(visit);
     res.writeHead(200, {
         'Content-Type': 'image/gif',
         'Content-Length': trackImg.length
     })
-    console.log(`${req.method} ${req.url} - ${req.socket.remoteAddress} - ${req.clientIp}`);
-    upload(req.clientIp || req.socket.remoteAddress, req.path);
     res.end(trackImg)
 })
 app.listen(process.env.port, () => {
