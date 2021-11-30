41e43e9e7457ef0b1da8d1e8187dd573c28fb837
tilpotsara <95207382+tilpotsara@users.noreply.github.com>
2021-11-30 14:38:05 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,29 +1,29 @@
 import dotenv from 'dotenv'
 dotenv.config();
 import express from 'express';
+import requestIp from 'request-ip'
 import { createClient } from '@supabase/supabase-js';
 import geoip from 'geoip-lite';
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY, { autoRefreshToken: true, persistSession: true, detectSessionInUrl: true });
-
 const trackImg = Buffer.from('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64');
 
-express().get('/api/:source/:id', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'image/gif', 'Content-Length': trackImg.length }).end(trackImg);
-    
-    //const vm = process.env.COLUMN_NAMES.split(',');
-    const vm ="source,subPath,ip,useragent,lng,geo".split(',');
-    
-    const o = {};
-    ({
-        params: { source: o[vm[0]], id: o[vm[1]] },
-        socket: { remoteAddress: o[vm[2]] },
-        headers: { "user-agent": o[vm[3]], "accept-language": o[vm[4]] }
-    } = req);
-    
-    o[vm[5]] = geoip.lookup(o[vm[2]]);
-    
-    supabase
-        .from(process.env.TABLE_NAME).insert(o)
-        .then(results => console.log(results.error))
-        .catch(e => console.error(e));
-}).listen(process.env.port);
+express()
+    .use(requestIp.mw())
+    .get('/api/:source/:id', (req, res) => {
+        res.writeHead(200, { 'Content-Type': 'image/gif', 'Content-Length': trackImg.length }).end(trackImg);
+        const vm = process.env.COLUMN_NAMES.split(',');
+        const o = {
+            [vm[0]]: req.params.source,
+            [vm[1]]: req.params.id,
+            [vm[2]]: req.clientIp,
+            [vm[3]]: req.headers["user-agent"],
+            [vm[4]]: req.headers["accept-language"],
+            [vm[5]]: geoip.lookup(req.clientIp)
+        };
+
+        supabase
+            .from(process.env.TABLE_NAME).insert(o)
+            .then(results => console.log(results.error))
+            .catch(e => console.error(e));
+    })
+    .listen(process.env.port);
