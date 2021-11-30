05cbd3f02ca5b0d68de2cabf0af1a1e60b627765
tilpotsara <95207382+tilpotsara@users.noreply.github.com>
2021-11-30 13:48:31 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,49 +1,27 @@
 import dotenv from 'dotenv'
 dotenv.config();
-import express from 'express'
-import requestIp from 'request-ip'
+import express from 'express';
 import { createClient } from '@supabase/supabase-js';
 import geoip from 'geoip-lite';
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY, { autoRefreshToken: true, persistSession: true, detectSessionInUrl: true });
 
-const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY,
-    {
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: true
-    })
-const app = express()
-app.use(requestIp.mw())
-async function upload(opened) {
-    const { data, error } = await supabase
-        .from('trackevents')
-        .insert([opened]);
-    console.log(error);
-}
 const trackImg = Buffer.from('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64');
-app.get('/api/:campaign/:id', (req, res) => {
-    const IP = req.clientIp || req.socket.remoteAddress;
-    console.log(`${req.method} ${req.url} - ${IP}`);
 
-    const visit = {
-        ipaddress: IP, source: req.path,
-        client: req.headers["user-agent"],
-        lng: req.headers["accept-language"],
-    }
-    if (IP) {
-        const geo = geoip.lookup(IP);
-        if (geo)
-            visit.geo = JSON.stringify(geo);
-    }
-
-    upload(visit);
-    res.writeHead(200, {
-        'Content-Type': 'image/gif',
-        'Content-Length': trackImg.length
-    })
-    res.end(trackImg)
-})
-app.listen(process.env.port, () => {
-    console.log(`listening at http://localhost:${process.env.port}`)
-})
+express().get('/api/:source/:id', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'image/gif', 'Content-Length': trackImg.length }).end(trackImg);
+    
+    const vm = process.env.COLUMN_NAMES.split(',');    
+    const o = {};
+    ({
+        params: { source: o[vm[0]], id: o[vm[1]] },
+        socket: { remoteAddress: o[vm[2]] },
+        headers: { "user-agent": o[vm[3]], "accept-language": o[vm[4]] }
+    } = req);
+    
+    o[vm[5]] = geoip.lookup(o[vm[2]]);
+    
+    supabase
+        .from(process.env.TABLE_NAME).insert(o)
+        .then(results => console.log(results.error))
+        .catch(e => console.error(e));
+}).listen(process.env.port);
